749710b621e64d0f3e13cb2e76f700ee5b0c89b1
Shortcut <hello@getshortcut.co>
2025-04-15 12:01:02 -0400

    Add modular share proposal functionality with proper design system integration

---
+++ dist 3/js/firebase-config.js
@@ -0,0 +1,9 @@
+// Firebase Configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyCC1gUxtUx8uVRiEP-ABSvhBVQB3uybDfQ",
+  authDomain: "shortcut-calculator.firebaseapp.com",
+  projectId: "shortcut-calculator",
+  storageBucket: "shortcut-calculator.firebasestorage.app",
+  messagingSenderId: "968175176487",
+  appId: "1:968175176487:web:3cc778f0ac0b9963973702"
+}; 
